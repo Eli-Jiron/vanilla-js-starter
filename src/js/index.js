5abebf04038d29c394a4e6be83fcdc3989b73854
Eli-Jiron <elianguevara28@gmail.com>
2024-06-03 14:45:03 -0600

    Funcion: subir datos del input a la api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-// Inserte el código aquí
+import { subirTarea } from "./subirTareas.js";
+
+let dato = document.getElementById('inputTarea');
+let btnEnviar = document.getElementById('btnAgregar');
+
+class tarea {
+    method = 'POST';
+    headers = {
+    'Content-Type': 'application/json',
+    };
+    body = '';
+
+    constructor (tarea) {
+        this.body = JSON.stringify({tarea});
+    }
+}
+
+btnEnviar.addEventListener('click' , function () {
+    let nuevaTarea = new tarea(dato.value);
+    subirTarea(nuevaTarea)
+});
