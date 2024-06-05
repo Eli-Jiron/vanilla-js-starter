1a2f00a3d85b4de28a997e7ae077065f6e9e80a2
Eli-Jiron <elianguevara28@gmail.com>
2024-06-04 21:11:12 -0600

    Funcion: contador de tareas completadas

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,39 @@
 import { subirTarea } from "./post.js";
 import { recibirTareas } from "./get.js";
 import { eliminarTarea } from "./delete.js";
+import { editarEstado } from "./put.js";
 
 let dato = document.getElementById('inputTarea');
 let btnAgregar = document.getElementById('btnAgregar');
 let listaTareas = document.getElementById('listaTareas');
-let mensaje = document.getElementById('mensaje');
+
+async function tareasCompletadas() {
+    let contador = document.getElementById('contador');
+    let promesa = await recibirTareas();
+    promesa.forEach(e => {
+        if (e.estado === 'completo') {
+            contador.textContent = parseInt(contador.textContent)+1;
+        }
+    });
+}
+
+async function checklist(id) {
+    let promesa = await recibirTareas();
+    promesa.forEach(e => {
+        if (e.id === id) {
+            if (e.estado === 'incompleto') {
+                editarEstado(id, 'completo');
+            } else {
+                editarEstado(id, 'incompleto');
+            }
+        }
+    });
+}
 
 cargarTareas();
 async function cargarTareas() {
-let promesaCumplida = await recibirTareas();
-
-promesaCumplida.forEach(e => {
+let promesa = await recibirTareas();
+promesa.forEach(e => {
     let border = document.createElement('div');
     let divPadre = document.createElement('div');
     let checkbox = document.createElement('input');
@@ -22,10 +44,15 @@ promesaCumplida.forEach(e => {
     checkbox.type = 'checkbox';
     button.textContent = 'Borrar';
 
-    checkbox.className = 'checkboxStyle'
+    if (e.estado === 'incompleto') {
+        checkbox.className = 'checkboxStyle';
+    } else {
+        checkbox.className = 'check';
+    }
     button.className = 'btnStyle'
     border.className = 'tareasBorder';
     divPadre.className = 'tareas';
+    checkbox.id = e.id;
     button.id = e.id;
 
     divTarea.appendChild(textoTarea); 
@@ -36,13 +63,20 @@ promesaCumplida.forEach(e => {
 
     listaTareas.appendChild(border);
 
+    checkbox.addEventListener('click', function () {
+        checklist(checkbox.id);
+    })
+
     button.addEventListener('click', function () {
         eliminarTarea(button.id);
     })
+
 });
+tareasCompletadas();
 }
 
 btnAgregar.addEventListener('click' , function () {
+    let mensaje = document.getElementById('mensaje');
     if (dato.value.trim() !== '') {
         subirTarea(dato.value);
     } else {
